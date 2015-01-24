500ffa72c8f702f70e45705f4771358a790da5e8
Darcy Cartmill <darcy.cartmill@gmail.com>
2015-01-24 18:54:18 -0500

    TEST
    YES

--- Assets/Item/CanScript.cs
+++ Assets/Item/CanScript.cs
@@ -6,6 +6,17 @@ public class CanScript : ItemScript {
 	
 	public override void ApplyEffect(){}
 
-	public override void OnPickup(){}
+	public override void OnPickup(){
+
+        //If user owns a Can Opener
+        if (GameObject.Find("Player").GetComponent<InventoryManager>().KillItemByName("Can Opener"))
+        {
+            // DESTROY THE CAN
+
+            // EXECUTE EVENT WITH HEAD DROPPING OUT
+
+            // DROP OUT STATUE FROM HEAD
+        }
+    }
 
 }
