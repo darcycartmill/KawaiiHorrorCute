c6018a9a40e49ef20c80f3c766b87e324c447ff7
Speared <speare.david@gmail.com>
2015-01-24 18:16:46 -0500

    flashy spheres for items

---
+++ Assets/Item/PulsingLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulsingLight : MonoBehaviour {
+	Light myLight;
+	float _minIntensity = 0;
+	float _maxIntensity = 0.1f;
+
+	bool intensifying = false;
+
+	// Use this for initialization
+	void Start () {
+		myLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!intensifying){
+			myLight.intensity -= Time.deltaTime;
+		}else{
+			myLight.intensity += Time.deltaTime;
+		}
+
+
+		if(myLight.intensity <= _minIntensity || myLight.intensity >= _maxIntensity){
+			intensifying = !intensifying;
+		}
+	}
+}
