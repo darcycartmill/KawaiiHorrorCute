a2412b392f74bfa8e17fb7724bd87f0d89821024
Speared <speare.david@gmail.com>
2015-01-24 10:25:16 -0500

    very little done
    A test scene, some test items and like 5 lines worth of an inventory
    manager. Look what a productive team member I am

---
+++ Assets/Character/InventoryManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryManager : MonoBehaviour {
+
+	public Transform heldItemSlot;
+
+	ItemScript heldItem;
+
+	int inventorySlots = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//apply held effects, like the lighter remaining on
+		if(heldItem != null){
+			heldItem.HeldEffect();
+		}
+
+		float activateItemButton = Input.GetAxis("Fire1");
+		float switchItemButton = Input.GetAxis("Mouse ScrollWheel");
+	}
+}
