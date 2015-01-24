450587c9f474f9d77bb9f4da778a192462af9193
Speared <speare.david@gmail.com>
2015-01-24 15:08:00 -0500

    Doors in their most basic form

---
+++ Assets/Character/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+//an atempt to make a player that looks like it has footsteps
+public class MovementScript : MonoBehaviour {
+	public Transform cameraTrans;
+
+	float _maxSpeed;
+	float _accelSpeed;
+
+	float _stepHeight;
+
+	bool _stepping = false;
+	Vector3 _moveVec;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float _horizontal = Input.GetAxis("Horizontal");
+		float _vertical = Input.GetAxis("Vertical");
+		Vector3 inputVec = new Vector3(_horizontal, 0, _vertical);
+		//this is an experiment in character movment
+		if(!_stepping){
+			_moveVec = inputVec;
+		}else{
+
+		}
+	}
+}
---
+++ Assets/Door/ShoveableByPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoveableByPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			Vector3 playerPos = GameObject.Find("Player").transform.position;
+			rigidbody.AddExplosionForce(20, playerPos, 99);
+		}
+	}
+	
+}
