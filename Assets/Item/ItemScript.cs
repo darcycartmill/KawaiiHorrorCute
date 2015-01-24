d9f328cbde6283219f7ea9e854dbd52782504f33
darcycartmill <darcy.cartmill@gmail.com>
2015-01-24 13:42:06 -0500

    Adding Item Name to ItemScript
    Item Name added as property

--- Assets/Item/ItemScript.cs
+++ Assets/Item/ItemScript.cs
@@ -7,8 +7,16 @@ public class ItemScript : MonoBehaviour {
 
 	public virtual void ApplyEffect(){}
 
+	private string _itemName;
+
+	public string ItemName {
+		get { return _itemName; }
+		set { _itemName = value; }
+	}
+
 	// Use this for initialization
 	void Start () {
+		_itemName = "Empty Item";
 	
 	}
 	
