5577bf5177bf2196e19c5fab4bab21b6ecb2b6b8
Speared <speare.david@gmail.com>
2015-01-24 22:03:18 -0500

    enemys
    spooky scary

---
+++ Assets/Enemy/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	NavMeshAgent myAgent;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		myAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myAgent.SetDestination(player.transform.position);
+	}
+}
