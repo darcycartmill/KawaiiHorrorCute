096ba5ba1e8fd7be6ef3ad07f2e604dfde566d8a
Speared <speare.david@gmail.com>
2015-01-25 14:02:57 -0500

    last commit

---
+++ Assets/Item/projectile/FadeLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeLight : MonoBehaviour {
+
+	public Light myLight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myLight.intensity -= Time.deltaTime;
+		if(myLight.intensity <= 0){
+			Destroy(gameObject);
+		}
+	}
+}
