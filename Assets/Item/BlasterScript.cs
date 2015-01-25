2d6c823fca028d6a0416511454ac5175847d01f0
Speared <speare.david@gmail.com>
2015-01-24 21:09:42 -0500

    boom
    boom

---
+++ Assets/Item/BlasterScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlasterScript : ItemScript {
+
+	GameObject powerbar;
+	
+	public GameObject shootMe;
+
+	float _energy;
+	float _energyLossRate = 5;
+	float _energyGainRate = 2;
+
+	float _energyPerShot = 20;
+
+	void Start(){
+		powerbar = GameObject.Find("Bar");
+		_energy = 100;
+	}
+	
+	public override void HeldEffect(){
+		Vector3 scale = new Vector3(1, _energy / 100f, 1);
+		if(powerbar != null){
+			powerbar.transform.localScale = scale;
+		}
+	}
+	
+	public override void OnPickup ()
+	{
+
+	}
+	
+	public override void OnDrop ()
+	{
+
+	}
+	
+	public override void ApplyEffect(){
+		if(_energy > _energyPerShot){
+			_energy -= _energyPerShot;
+		}else{
+			return;
+		}
+
+		GameObject projectile = Instantiate(shootMe, transform.position, transform.rotation) as GameObject;
+		projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * 500);
+	}
+	
+	void Update(){
+		_energy += Time.deltaTime * _energyGainRate;
+		_energy = Mathf.Clamp(_energy, 0, 100);
+	}
+}
